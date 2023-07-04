1334e0145d8226f6b7f184762d477b2a836398dc
aifuxi <aifuxi.js@gmail.com>
2023-07-04 08:47:46 +0800

    feat(tailwindcss): 配置tailwindcss插件

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,18 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
+    extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    require('tailwindcss-theming')({
+      variants: {
+        dark: true,
+        light: true,
+      },
+      themes: false,
+    }),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('tailwindcss-debug-screens'),
+  ],
+};
