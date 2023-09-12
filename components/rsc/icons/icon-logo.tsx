0b4c37957f7e86cccedc93b06eead1578af2fefc
aifuxi <aifuxi.js@gmail.com>
2023-09-13 00:22:07 +0800

    feat: 网站logo替换

---
+++ components/rsc/icons/icon-logo.tsx
@@ -0,0 +1,25 @@
+import { SVGProps } from 'react';
+
+export const IconLogo = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="1em"
+    height="1em"
+    fill="none"
+    viewBox="0 0 64 64"
+    {...props}
+  >
+    <g clipPath="url(#a)">
+      <path fill="hsl(var(--primary))" d="M0 0h64v48L48 64H0V0Z" />
+      <path
+        fill="hsl(var(--primary-foreground))"
+        d="M20.928 51V15.624h25.104v6.384H28.416v8.352h15.456v6.144H28.416V51h-7.488Z"
+      />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="hsl(var(--primary-foreground))" d="M0 0h64v64H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
--- components/rsc/icons/index.ts
+++ components/rsc/icons/index.ts
@@ -2,6 +2,14 @@ import { IconBilibili } from './icon-bilibili';
 import { IconEmail } from './icon-email';
 import { IconGithub } from './icon-github';
 import { IconJuejin } from './icon-juejin';
+import { IconLogo } from './icon-logo';
 import { IconWechat } from './icon-wechat';
 
-export { IconBilibili, IconEmail, IconGithub, IconJuejin, IconWechat };
+export {
+  IconBilibili,
+  IconEmail,
+  IconGithub,
+  IconJuejin,
+  IconWechat,
+  IconLogo,
+};
