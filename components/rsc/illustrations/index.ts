0cc146f4f101d37917ace3bd58fffaa0b98bc4e9
aifuxi <aifuxi.js@gmail.com>
2023-09-14 01:54:27 +0800

    refactor: 文章列表页样式重构

--- components/rsc/illustrations/index.ts
+++ components/rsc/illustrations/index.ts
@@ -1,3 +1,4 @@
+import { BarCode } from './bar-code';
 import NoDataIllustration from './no-data-illustration';
 import NotFound404Illustration from './not-found-404-illustration';
 import Unauthorized401Illustration from './unauthorized-401-illustration';
@@ -6,4 +7,5 @@ export {
   NoDataIllustration,
   NotFound404Illustration,
   Unauthorized401Illustration,
+  BarCode,
 };
