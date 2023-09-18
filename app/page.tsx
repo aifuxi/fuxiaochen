fb2bc9932749e49150eeb0953fbf236c74b4fb7e
aifuxi <aifuxi.js@gmail.com>
2023-09-18 21:22:23 +0800

    feat: 新增分页组件和重构获取server数据逻辑

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
 import { Metadata } from 'next';
-import Link from 'next/link';
 
-import { PageTitle } from '@/components/rsc';
-import { DEFAULT_PAGE, LATEST_ARTICLES_PAGE_SIZE, NICKNAME } from '@/constants';
+import { NICKNAME } from '@/constants';
 import { cn } from '@/utils';
-
-import ArticleItem from './articles/article-item';
-import { getServerSideArticles } from './fetch-data';
 
 export const revalidate = 60;
 
@@ -16,66 +11,25 @@ export const metadata: Metadata = {
 };
 
 export default async function HomePage() {
-  const res = await getServerSideArticles({
-    page: DEFAULT_PAGE,
-    published: true,
-    pageSize: LATEST_ARTICLES_PAGE_SIZE,
-  });
-  const articles = res?.data;
-
   return (
     <div className="container">
       <div className="flex flex-col h-screen justify-center items-center">
         <img
-          src="/images/nyan-cat.gif"
+          src="/images/nyan-cat.webp"
           alt="Nyan Cat"
           className={cn('w-full h-auto')}
         />
         <div className="flex">
-          <h1
+          <h2
             className={cn(
               'flex font-semibold',
               'text-xl sm:text-4xl 2xl:text-5xl',
             )}
           >
             F西，努力做一个更好的程序员。
-          </h1>
-          <span
-            className={cn('ml-2 w-2 h-full', 'bg-gray-900 dark:bg-white')}
-          />
+          </h2>
         </div>
       </div>
-      {renderLatestArticles()}
     </div>
   );
-
-  function renderLatestArticles() {
-    if (!articles?.length) {
-      return null;
-    }
-
-    return (
-      <div className="flex flex-col space-y-8 pt-8">
-        <PageTitle title="最新文章" />
-        <ul className="flex flex-col space-y-10">
-          {articles?.map((article) => (
-            <li key={article.id}>
-              <ArticleItem article={article} />
-            </li>
-          ))}
-        </ul>
-        <div className="text-base font-medium leading-6 text-right">
-          <Link
-            className={cn(
-              'text-primary-500 font-medium ',
-              'hover:text-primary-600 dark:hover:text-primary-400',
-            )}
-            href={`/articles`}
-          >
-            全部文章 →
-          </Link>
-        </div>
-      </div>
-    );
-  }
 }
--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  experimental: {
+    serverActions: true,
+  },
   images: {
     remotePatterns: [
       {
