5ee5dfecc51af6cff8036658381cb74195abd6a9
aifuxi <aifuxi.js@gmail.com>
2023-10-05 10:25:31 +0800

    feat: 编辑标签

--- app/_actions/tag.ts
+++ app/_actions/tag.ts
@@ -4,7 +4,7 @@ import { revalidatePath } from 'next/cache';
 
 import { DEFAULT_PAGE_SIZE } from '@/constants';
 import prisma from '@/libs/prisma';
-import { CreateTagRequest } from '@/types';
+import { CreateTagRequest, UpdateTagRequest } from '@/types';
 
 export async function getTagsWithArticleCountAction() {
   const tags = await prisma.tag.findMany({
@@ -24,45 +24,6 @@ export async function getTagsWithArticleCountAction() {
   });
 
   return tags;
-}
-
-export async function adminGetTagsAction(params: { page: number }) {
-  const take = DEFAULT_PAGE_SIZE;
-  const skip = (params.page - 1) * DEFAULT_PAGE_SIZE;
-
-  let tags = await prisma.tag.findMany({
-    orderBy: {
-      createdAt: 'desc',
-    },
-    include: {
-      articles: {
-        where: {
-          published: true,
-        },
-        orderBy: {
-          createdAt: 'desc',
-        },
-      },
-    },
-  });
-
-  const count = await prisma.tag.count({});
-
-  tags = tags?.slice(skip, skip + take);
-  const total = count || 0;
-
-  revalidatePath('/admin/tag');
-  return { tags, total };
-}
-export async function adminCreateTagAction(params: CreateTagRequest) {
-  const tag = await prisma.tag.create({
-    data: { ...params },
-    include: { articles: true },
-  });
-
-  revalidatePath('/admin/tag');
-
-  return tag;
 }
 
 export async function getTagArticlesByFriendlyURLAction(params: {
@@ -100,6 +61,74 @@ export async function getTagByFriendlyURLAction(friendlyURL: string) {
       friendlyUrl: friendlyURL,
     },
   });
+
+  return tag;
+}
+
+export async function adminGetTagsAction(params: { page: number }) {
+  const take = DEFAULT_PAGE_SIZE;
+  const skip = (params.page - 1) * DEFAULT_PAGE_SIZE;
+
+  let tags = await prisma.tag.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      articles: {
+        where: {
+          published: true,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+      },
+    },
+  });
+
+  const count = await prisma.tag.count({});
+
+  tags = tags?.slice(skip, skip + take);
+  const total = count || 0;
+
+  revalidatePath('/admin/tag');
+  return { tags, total };
+}
+
+export async function adminCreateTagAction(params: CreateTagRequest) {
+  const tag = await prisma.tag.create({
+    data: { ...params },
+    include: { articles: true },
+  });
+
+  revalidatePath('/admin/tag');
+
+  return tag;
+}
+
+export async function adminEditTagAction(
+  tagID: string,
+  params: UpdateTagRequest,
+) {
+  const tag = await prisma.tag.update({
+    where: {
+      id: tagID,
+    },
+    data: { ...params },
+  });
+
+  revalidatePath('/admin/tag');
+
+  return tag;
+}
+
+export async function adminDeleteTagAction(tagID: string) {
+  const tag = await prisma.tag.delete({
+    where: {
+      id: tagID,
+    },
+  });
+
+  revalidatePath('/admin/tag');
 
   return tag;
 }
