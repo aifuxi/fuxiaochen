ff433a52ee3d049e532c64541369c05220494859
aifuxi <aifuxi.js@gmail.com>
2023-11-23 21:49:24 +0800

    feat: 优化环境变量引入方式

--- libs/ali-oss.ts
+++ libs/ali-oss.ts
@@ -1,14 +1,16 @@
 import OSS from 'ali-oss';
 
+import { env } from './env.mjs';
+
 const globalForAliOSS = global as unknown as { aliOSS: OSS | undefined };
 
 export const aliOSS =
   globalForAliOSS.aliOSS ??
   new OSS({
-    accessKeyId: process.env.OSS_ACCESS_KEY_ID ?? '',
-    accessKeySecret: process.env.OSS_ACCESS_KEY_SECRET ?? '',
-    region: process.env.OSS_REGION ?? '',
-    bucket: process.env.OSS_BUCKET ?? '',
+    accessKeyId: env.OSS_ACCESS_KEY_ID ?? '',
+    accessKeySecret: env.OSS_ACCESS_KEY_SECRET ?? '',
+    region: env.OSS_REGION ?? '',
+    bucket: env.OSS_BUCKET ?? '',
   });
 
-if (process.env.NODE_ENV !== 'production') globalForAliOSS.aliOSS = aliOSS;
+if (env.NODE_ENV !== 'production') globalForAliOSS.aliOSS = aliOSS;
