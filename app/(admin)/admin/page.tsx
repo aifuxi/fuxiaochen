c3ff74dd780a2c40a4e644b01d6c437ce3890909
aifuxi <aifuxi.js@gmail.com>
2023-11-30 21:44:44 +0800

    style: 优化布局样式

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,3 +1,11 @@
+import { Flex, Heading } from '@radix-ui/themes';
+
 export default function AdminPage() {
-  return <>ADMIN</>;
+  return (
+    <Flex direction={'column'} gap={'4'}>
+      <Heading size={'6'} as="h4">
+        Dashboard
+      </Heading>
+    </Flex>
+  );
 }
