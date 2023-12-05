98fb4889194bbe23481f79d90a6fb6fc5b90c751
aifuxi <aifuxi.js@gmail.com>
2023-12-06 00:26:31 +0800

    chore: 调整字体引入方式

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,13 @@ import { ThemeProvider } from '@/components/providers';
 import { WEBSITE } from '@/constants';
 import '@/styles/global.css';
 
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+import '@fontsource/poppins/800.css';
+import '@fontsource/poppins/900.css';
+
 export const metadata: Metadata = {
   title: {
     template: `%s - ${WEBSITE}`,
