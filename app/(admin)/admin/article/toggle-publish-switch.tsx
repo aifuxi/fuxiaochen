cfe066995c4be343197c53d67918de4baeb78138
aifuxi <aifuxi.js@gmail.com>
2023-12-06 23:33:18 +0800

    chore: 调整后台界面样式

--- app/(admin)/admin/article/toggle-publish-switch.tsx
+++ app/(admin)/admin/article/toggle-publish-switch.tsx
@@ -1,9 +1,9 @@
 'use client';
 
 import { type Article } from '@prisma/client';
-import { Switch } from '@radix-ui/themes';
 
 import { toggleArticlePublish } from '@/app/_actions/article';
+import { Switch } from '@/components/ui/switch';
 
 type Props = {
   article: Article;
@@ -13,8 +13,6 @@ export function TogglePublishSwitch({ article }: Props) {
   return (
     <Switch
       checked={article.published}
-      color="gray"
-      highContrast
       onCheckedChange={async () => {
         await toggleArticlePublish(article.id);
       }}
--- app/(admin)/admin/tag/edit-tag-button.tsx
+++ app/(admin)/admin/tag/edit-tag-button.tsx
@@ -1,10 +1,19 @@
 'use client';
 
 import { type Tag } from '@prisma/client';
-import { Button, Dialog, IconButton, Text, TextField } from '@radix-ui/themes';
+import { Text, TextField } from '@radix-ui/themes';
 import { PencilIcon } from 'lucide-react';
 
 import { updateTag } from '@/app/_actions/tag';
+import { Button } from '@/components/ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 
 type Props = {
   tag: Tag;
@@ -12,14 +21,16 @@ type Props = {
 
 export function EditTagButton({ tag }: Props) {
   return (
-    <Dialog.Root>
-      <Dialog.Trigger>
-        <IconButton color="gray" variant="solid" highContrast>
-          <PencilIcon />
-        </IconButton>
-      </Dialog.Trigger>
-      <Dialog.Content>
-        <Dialog.Title>编辑标签</Dialog.Title>
+    <Dialog>
+      <DialogTrigger>
+        <Button size={'icon'}>
+          <PencilIcon size={16} />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>编辑标签</DialogTitle>
+        </DialogHeader>
         <form action={updateTag}>
           <div className="flex flex-col gap-3">
             <label>
@@ -46,21 +57,12 @@ export function EditTagButton({ tag }: Props) {
             </label>
             <input type="hidden" name="id" value={tag.id} />
           </div>
-          <div className="flex gap-3 mt-4 justify-end">
-            <Dialog.Close>
-              <Button variant="soft" color="gray">
-                取消
-              </Button>
-            </Dialog.Close>
-            <Dialog.Close>
-              <Button type="submit" color="gray" highContrast>
-                保存
-              </Button>
-            </Dialog.Close>
-          </div>
+          <DialogFooter>
+            <Button>保存</Button>
+          </DialogFooter>
         </form>
-      </Dialog.Content>
-    </Dialog.Root>
+      </DialogContent>
+    </Dialog>
   );
 }
 
