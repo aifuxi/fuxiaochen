563eab5b7c5c39f7113bcd4c9c24a4c74c0fef48
aifuxi <aifuxi.js@gmail.com>
2023-12-08 20:32:21 +0800

    chore: 后台dashboard空态页

--- app/(admin)/admin/page.tsx
+++ app/(admin)/admin/page.tsx
@@ -1,9 +1,21 @@
+import { IllustrationConstruction } from '@/components/illustrations';
+
 export default function AdminPage() {
   return (
-    <div className="flex flex-col gap-4">
+    <div className="h-screen flex flex-col gap-4">
       <h2 className="text-3xl font-semibold tracking-tight transition-colors">
         Dashboard
       </h2>
+
+      <div className="flex-1 grid place-items-center">
+        <div className="grid gap-8">
+          <IllustrationConstruction className="w-[300px] h-[300px]" />
+
+          <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight text-center">
+            页面建设中
+          </h3>
+        </div>
+      </div>
     </div>
   );
 }
