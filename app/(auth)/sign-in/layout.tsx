3ac367b53880baf2484436049b91a977b0812fd3
aifuxi <aifuxi.js@gmail.com>
2023-12-13 00:50:01 +0800

    feat: 已登录时访问登录页面重定向到后台管理首页

--- app/(auth)/sign-in/layout.tsx
+++ app/(auth)/sign-in/layout.tsx
@@ -1,9 +1,21 @@
 import { type Metadata } from 'next';
+import { redirect } from 'next/navigation';
+
+import { PATHS } from '@/constants/path';
+import { auth } from '@/libs/auth';
 
 export const metadata: Metadata = {
   title: '用户登录',
 };
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default async function Layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+  if (session?.user.email) {
+    redirect(PATHS.ADMIN_HOME);
+  }
   return <>{children}</>;
 }
