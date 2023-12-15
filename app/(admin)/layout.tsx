1dc23fbb7d6b2d1a450315e3b582006cc1b8b931
aifuxi <aifuxi.js@gmail.com>
2023-12-15 20:08:47 +0800

    feat(admin): 后台新增切换主题按钮

--- app/(admin)/layout.tsx
+++ app/(admin)/layout.tsx
@@ -1,5 +1,6 @@
 import { SideNav } from '@/components/navbar';
 import { SignOutButton } from '@/components/sign-out-button';
+import { SwitchTheme } from '@/components/switch-theme';
 
 import { auth } from '@/libs/auth';
 
@@ -29,6 +30,10 @@ export default async function AdminLayout({
       </div>
       <div className="h-screen overflow-scroll flex flex-1 p-8 flex-col">
         {children}
+      </div>
+
+      <div className="fixed w-12 h-12 grid place-content-center right-12 top-6">
+        <SwitchTheme variant={'outline'} />
       </div>
     </div>
   );
