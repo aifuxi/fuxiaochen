8e3929e54bc6b06382a1c9cbf2d1c6580d9a1c2e
aifuxi <aifuxi.js@gmail.com>
2023-12-25 22:55:36 +0800

    feat(site): 文章内容生成标题链接，链接前添加icon

--- components/bytemd/viewer.tsx
+++ components/bytemd/viewer.tsx
@@ -5,7 +5,6 @@ import React from 'react';
 import { Viewer } from '@bytemd/react';
 
 import { plugins, sanitize } from './config';
-import { tocPlugin } from './plugin';
 
 type Props = {
   content: string;
@@ -13,11 +12,5 @@ type Props = {
 
 // 实现目录功能，参考了这个 Issue：https://github.com/pd4d10/hashmd/issues/126
 export function BytemdViewer({ content }: Props) {
-  return (
-    <Viewer
-      value={content}
-      plugins={[...plugins, tocPlugin()]}
-      sanitize={sanitize}
-    />
-  );
+  return <Viewer value={content} plugins={plugins} sanitize={sanitize} />;
 }
