bfcbc5f07795e62079a9b5222c0df4525e71a0d7
fuxiaochen <aifuxi.js@gmail.com>
2024-02-29 23:07:41 +0800

    feat: 🎸 重新修改logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ import '@/styles/global.css';
 
 export const metadata: Metadata = {
   title: {
-    template: `%s - ${WEBSITE}`,
+    template: `%s · ${WEBSITE}`,
     default: `${WEBSITE}`,
   },
   description: `${SLOGAN}`,
@@ -31,32 +31,10 @@ export default function RootLayout({
     <html suppressHydrationWarning lang="zh-CN">
       <head>
         <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
           rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
+          type="image/svg+xml"
+          href="/images/fuxiaochen-dark.svg"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-
-        {/* Google Search Console 验证 */}
-        <meta
-          name="google-site-verification"
-          content="UT_Si71yQw3RQF1ALRd33CyBjR56msBkQRXe20WP4L0"
-        />
-
-        {/* 百度站长验证 */}
-        <meta name="baidu-site-verification" content="codeva-3wpRQCVZIT" />
       </head>
 
       <body className={'debug-screens'}>
