70a53948aafb0d57d725019823b3586323603cd4
fuxiaochen <aifuxi.js@gmail.com>
2024-02-29 22:37:58 +0800

    style: 💄 清除首页杂乱样式

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,42 +8,32 @@ import { NICKNAME } from '@/constants/info';
 import { PATHS } from '@/constants/path';
 
 import { AboutMe } from './_components/about-me';
-import { CodeBlock } from './_components/code-block';
 import { MyKillsets } from './_components/my-killsets';
 import { ThingsILove } from './_components/things-i-love';
 
 export default function HomePage() {
   return (
     <div>
-      <div className="relative">
-        <div
-          style={{ clipPath: 'polygon(70% 0, 100% 0, 100% 100%, 50% 50%)' }}
-          className="absolute inset-0 -inset-y-1/2 bg-primary z-[-1]"
-        ></div>
-        <div className="container flex flex-col lg:flex-row lg:justify-between items-center pt-20">
-          <div className="flex-1 grid gap-2 pb-8 md:pb-0">
-            <p className="text-muted-foreground tracking-widest">你好，我是</p>
-            <strong className="text-5xl tracking-widest">{NICKNAME}</strong>
-            <p className="text-xl text-muted-foreground tracking-widest">
-              一名前端开发工程师
-            </p>
-            <p className="text-sm text-muted-foreground tracking-widest">
-              在这个网站记录我的成长，努力成为一个更好的程序员。
-            </p>
-            <div className="my-8">
-              <Link
-                href={PATHS.SITE_ARTICLES}
-                className={cn(
-                  buttonVariants({ variant: 'default' }),
-                  'px-8 font-semibold',
-                )}
-              >
-                👉 查看全部文章
-              </Link>
-            </div>
-          </div>
-          <div className="flex-1 w-full flex justify-center">
-            <CodeBlock />
+      <div className="container flex flex-col lg:flex-row lg:justify-between items-center pt-20 min-h-screen">
+        <div className="flex-1 grid gap-2 pb-8 md:pb-0">
+          <p className="text-muted-foreground tracking-widest">你好，我是</p>
+          <strong className="text-5xl tracking-widest">{NICKNAME}</strong>
+          <p className="text-xl text-muted-foreground tracking-widest">
+            一名前端开发工程师
+          </p>
+          <p className="text-sm text-muted-foreground tracking-widest">
+            在这个网站记录我的成长，努力成为一个更好的程序员。
+          </p>
+          <div className="my-8">
+            <Link
+              href={PATHS.SITE_ARTICLES}
+              className={cn(
+                buttonVariants({ variant: 'default' }),
+                'px-8 font-semibold',
+              )}
+            >
+              👉 查看全部文章
+            </Link>
           </div>
         </div>
       </div>
