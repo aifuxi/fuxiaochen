3267e71dc6c374de288f4095a5eb4725c0493235
fuxiaochen <aifuxi.js@gmail.com>
2024-03-05 23:52:02 +0800

    refactor: 💡 删除不用的文件

--- app/(auth)/_sign-up/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { type Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: '创建用户',
-};
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
