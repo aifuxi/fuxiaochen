d2abb83957d99433c670a46a8d7fba3d1735e7e9
fuxiaochen <aifuxi.js@gmail.com>
2024-03-09 21:14:13 +0800

    refactor: 💡 抽离编辑文章页

--- app/admin/article/edit/[id]/page.tsx
+++ app/admin/article/edit/[id]/page.tsx
@@ -1,37 +1,5 @@
-import Link from 'next/link';
+import { EditArticlePage } from '@/features/admin';
 
-import { getArticle } from '@/app/actions/article';
-
-import { PATHS } from '@/config';
-
-import { getTags } from '@/features/tag';
-
-import { EditForm } from './edit-form';
-
-export default async function AdminArticleCreate({
-  params,
-}: {
-  params: { id: string };
-}) {
-  const { tags } = await getTags();
-  const article = await getArticle(params.id);
-
-  return (
-    <div className="flex flex-col gap-4">
-      <div className="flex items-center gap-4">
-        <Link href={PATHS.ADMIN_ARTICLE}>
-          <h2 className="text-3xl font-semibold tracking-tight transition-colors text-foreground/80 hover:text-foreground ">
-            文章管理
-          </h2>
-        </Link>
-
-        <div>/</div>
-        <h2 className="text-3xl font-semibold tracking-tight transition-colors">
-          编辑文章
-        </h2>
-      </div>
-
-      <EditForm article={article ?? undefined} tags={tags} />
-    </div>
-  );
+export default function Page() {
+  return <EditArticlePage />;
 }
