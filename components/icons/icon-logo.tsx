0996be5192df5bdb5e274b2682cdffd6075d2e7e
fuxiaochen <aifuxi.js@gmail.com>
2024-03-10 01:40:04 +0800

    style: 💄 顶部导航栏样式优化

--- components/icons/icon-logo.tsx
+++ components/icons/icon-logo.tsx
@@ -1,20 +1,20 @@
-import { type SVGProps } from 'react';
+'use client';
 
-export const IconLogo = (props: SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="1em"
-    height="1em"
-    fill="none"
-    viewBox="0 0 48 48"
-    {...props}
-  >
-    <rect width={48} height={48} fill="hsl(var(--primary))" rx={12} />
-    <path
-      fill="hsl(var(--primary-foreground))"
-      fillRule="evenodd"
-      d="M13.176 9.422h21.621v7.217c0 .615-.498 1.113-1.113 1.113H13.176v-8.33Zm9.011 10.412h10.811v7.217c0 .615-.498 1.113-1.113 1.113h-9.698v-8.33Zm-9.011 0v18.744h6.093c.615 0 1.114-.498 1.114-1.114v-17.63h-7.207Z"
-      clipRule="evenodd"
+import { useTheme } from 'next-themes';
+
+import { WEBSITE } from '@/config';
+
+export const IconLogo = () => {
+  const { resolvedTheme } = useTheme();
+  return (
+    <img
+      src={
+        resolvedTheme === 'dark'
+          ? '/images/fuxiaochen-light.svg'
+          : '/images/fuxiaochen-dark.svg'
+      }
+      className="w-6 h-6"
+      alt={WEBSITE}
     />
-  </svg>
-);
+  );
+};
--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,35 +1,53 @@
+'use client';
+
 import Link from 'next/link';
 
+import { useScroll } from 'ahooks';
 import { GithubIcon } from 'lucide-react';
 
-import { cn } from '@/utils/helper';
+import { NICKNAME, PATHS, SOURCE_CODE_GITHUB_PAGE, WEBSITE } from '@/config';
 
-import { GITHUB_PAGE, NICKNAME, PATHS } from '@/config';
+import { cn } from '@/utils/helper';
 
 import { MobileNav } from './mobile-nav';
 import { NavList } from './nav-list';
 
 import { IconLogo } from '../icons';
+import { NextLink } from '../next-link';
 import { SwitchTheme } from '../switch-theme';
 import { Button } from '../ui/button';
 
-export function Navbar() {
+export const Navbar = () => {
+  const scroll = useScroll(window.document);
+
   return (
-    <header className="w-full sticky top-0 bg-background z-10 flex justify-center">
+    <header
+      className={cn(
+        'w-full sticky top-0 backdrop-blur transition-[background-color,border-width]  z-20 flex justify-center',
+        (scroll?.top ?? 0) > 60 && 'bg-background/50 border-b border-border/50',
+      )}
+    >
       <div className="w-full flex items-center h-16 p-4 sm:p-8 max-w-screen-xl">
-        <Link
+        <NextLink
           href={PATHS.SITE_HOME}
           className={cn('mr-4 hidden sm:flex')}
           aria-label={NICKNAME}
         >
-          <IconLogo className="w-10 h-10" />
-        </Link>
+          <IconLogo />
+          <span className="ml-2 font-semibold text-primary text-base">
+            {WEBSITE}
+          </span>
+        </NextLink>
         <NavList />
         <MobileNav />
         <div className="flex flex-1 sm:flex-none justify-end items-center gap-1">
           <SwitchTheme />
 
-          <Link href={GITHUB_PAGE} target="_blank" aria-label={GITHUB_PAGE}>
+          <Link
+            href={SOURCE_CODE_GITHUB_PAGE}
+            target="_blank"
+            aria-label={SOURCE_CODE_GITHUB_PAGE}
+          >
             <Button variant="ghost" size={'icon'} aria-label="Github Icon">
               <GithubIcon size={16} />
             </Button>
@@ -38,6 +56,6 @@ export function Navbar() {
       </div>
     </header>
   );
-}
+};
 
 export { SideNav } from './side-nav';
