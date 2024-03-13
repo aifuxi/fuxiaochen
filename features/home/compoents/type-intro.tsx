628865fa29e4a7072048043abb4528f44364c29c
fuxiaochen <aifuxi.js@gmail.com>
2024-03-13 20:43:29 +0800

    style: 💄 打字动画设置延时

--- features/home/compoents/type-intro.tsx
+++ features/home/compoents/type-intro.tsx
@@ -7,7 +7,13 @@ export const TypeIntro = () => {
   return (
     <TypeAnimation
       className="text-5xl tracking-widest"
-      sequence={['一名前端开发工程师 。', 1000, 'A Web <Developer /> .', 1000]}
+      sequence={[
+        500,
+        '一名前端开发工程师 。',
+        1000,
+        'A Web <Developer /> .',
+        1000,
+      ]}
       speed={10}
       repeat={Infinity}
     />
