149d114c851fa3bd4f22251b3d5681030a4ca769
fuxiaochen <aifuxi.js@gmail.com>
2024-03-14 21:01:51 +0800

    style: 💄 PageHeader增加边距

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -11,7 +11,10 @@ export default async function Page() {
 
   return (
     <div className="w-full flex flex-col justify-center px-6 md:max-w-screen-md  2xl:max-w-6xl  md:mx-auto pb-24 pt-8">
-      <PageHeader breadcrumbList={[PATHS.SITE_HOME, PATHS.SITE_BLOG]} />
+      <PageHeader
+        breadcrumbList={[PATHS.SITE_HOME, PATHS.SITE_BLOG]}
+        className="mb-9"
+      />
 
       <BlogList blogs={blogs} />
     </div>
--- app/(site)/snippet/page.tsx
+++ app/(site)/snippet/page.tsx
@@ -11,7 +11,10 @@ export default async function Page() {
 
   return (
     <div className="w-full flex flex-col justify-center px-6 md:max-w-screen-md 2xl:max-w-6xl md:px-0 md:mx-auto pb-24 pt-8">
-      <PageHeader breadcrumbList={[PATHS.SITE_HOME, PATHS.SITE_SNIPPET]} />
+      <PageHeader
+        breadcrumbList={[PATHS.SITE_HOME, PATHS.SITE_SNIPPET]}
+        className="mb-9"
+      />
 
       <SnippetList snippets={snippets} />
     </div>
