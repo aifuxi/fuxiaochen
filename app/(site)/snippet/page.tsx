efe3f9afa11887ab6e24f4edf4c9af527edc90f0
fuxiaochen <aifuxi.js@gmail.com>
2024-03-15 16:11:58 +0800

    feat: 🎸 新增getAllSnippets

--- app/(site)/snippet/page.tsx
+++ app/(site)/snippet/page.tsx
@@ -1,12 +1,12 @@
 import { PageHeader } from '@/components/page-header';
 
 import { PATHS } from '@/constants';
-import { SnippetList, getSnippets } from '@/features/snippet';
+import { SnippetList, getAllSnippets } from '@/features/snippet';
 
 export const revalidate = 60;
 
 export default async function Page() {
-  const { snippets } = await getSnippets();
+  const { snippets } = await getAllSnippets();
 
   return (
     <div className="w-full flex flex-col justify-center px-6 md:max-w-screen-md 2xl:max-w-6xl md:px-0 md:mx-auto pb-24 pt-8">
