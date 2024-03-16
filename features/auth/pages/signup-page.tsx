bab594c32f897a6aea112f725bb80409660dc234
fuxiaochen <aifuxi.js@gmail.com>
2024-03-16 14:48:07 +0800

    feat: 🎸 登录注册页面动画

--- features/auth/pages/signup-page.tsx
+++ features/auth/pages/signup-page.tsx
@@ -14,7 +14,7 @@ import { SignupForm } from '../components/signup-form';
 export const SignupPage = () => {
   return (
     <div className="w-screen h-screen grid place-content-center">
-      <Card className="w-[320px] sm:w-full sm:max-w-none sm:min-w-[360px] relative">
+      <Card className="w-[320px] sm:w-full sm:max-w-none sm:min-w-[360px] relative animate-fade-down">
         <CardHeader>
           <CardTitle className="flex justify-between items-center">
             邮箱注册 <NextLink href={PATHS.SITE_HOME}>回首页</NextLink>
