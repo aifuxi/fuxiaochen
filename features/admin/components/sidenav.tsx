8097b1a1dd03a062647376f4db3563bd56871b6b
fuxiaochen <aifuxi.js@gmail.com>
2024-03-17 15:43:39 +0800

    fix: 🐛 admin侧边栏无头像兜底

--- features/admin/components/sidenav.tsx
+++ features/admin/components/sidenav.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 
+import { PLACEHODER_TEXT } from '@/constants';
 import { SignOutButton } from '@/features/auth';
 import { auth } from '@/lib/auth';
 
@@ -12,20 +13,16 @@ export const Sidenav = async () => {
 
   return (
     <aside className="min-w-[256px] max-w-[256px] h-screen flex-col flex items-center py-12 px-8 bg-foreground">
-      {session?.user?.image && (
-        <Avatar className="w-14 h-14 border border-muted-foreground/10">
-          <AvatarImage
-            src={session?.user?.image}
-            alt={session?.user?.name ?? ''}
-          />
-          <AvatarFallback>N/A</AvatarFallback>
-        </Avatar>
-      )}
-
+      <Avatar className="w-14 h-14 border border-muted-foreground/10">
+        <AvatarImage
+          src={session?.user?.image ?? ''}
+          alt={session?.user?.name ?? PLACEHODER_TEXT}
+        />
+        <AvatarFallback>{PLACEHODER_TEXT}</AvatarFallback>
+      </Avatar>
       <h4 className="scroll-m-20 text-lg font-semibold tracking-tight mt-2 text-primary-foreground">
-        {session?.user?.name ?? ''}
+        {session?.user?.name ?? PLACEHODER_TEXT}
       </h4>
-
       <div className="w-full flex-col flex items-center mt-24 space-y-8">
         <Sidebar />
       </div>
