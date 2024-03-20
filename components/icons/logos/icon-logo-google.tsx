9481ce29bf6f05cd910c8dc298d44a7fb82b62ba
fuxiaochen <aifuxi.js@gmail.com>
2024-03-21 00:18:33 +0800

    feat: 🎸 更新关于页面

---
+++ components/icons/logos/icon-logo-google.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { cn } from '@/lib/utils';
+
+export const IconLogoGoogle = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLSpanElement>) => {
+  return (
+    <span
+      {...props}
+      className={cn('icon-[logos--google-icon]', className)}
+    ></span>
+  );
+};
