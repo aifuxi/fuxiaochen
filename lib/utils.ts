d2ca99a693893f015f5e4cb8c797c81262ceab6b
fuxiaochen <aifuxi.js@gmail.com>
2024-03-21 00:00:15 +0800

    fix: 🐛 修复移动端复制代码失败

--- lib/utils.ts
+++ lib/utils.ts
@@ -26,9 +26,11 @@ export const toSlug = (s: string) => {
 };
 
 export const copyToClipboard = (text: string) => {
+  // 实测 Clipboard API 在 iPhone 上不支持，可恶！
   if (navigator.clipboard) {
     navigator.clipboard
-      .writeText(text)
+      // 去除首尾空白字符
+      .writeText(text?.trim())
       .then(() => {
         showSuccessToast('已复制到粘贴板');
       })
@@ -36,7 +38,22 @@ export const copyToClipboard = (text: string) => {
         showErrorToast(error as string);
       });
   } else {
-    showErrorToast('浏览器不支持 Clipboard API');
+    // 以下代码来自：https://www.zhangxinxu.com/wordpress/2021/10/js-copy-paste-clipboard/
+    const textarea = document.createElement('textarea');
+    document.body.appendChild(textarea);
+    // 隐藏此输入框
+    textarea.style.position = 'fixed';
+    textarea.style.clip = 'rect(0 0 0 0)';
+    textarea.style.top = '10px';
+    // 赋值，手动去除首尾空白字符
+    textarea.value = text?.trim();
+    // 选中
+    textarea.select();
+    // 复制
+    document.execCommand('copy', true);
+    showSuccessToast('已复制到粘贴板');
+    // 移除输入框
+    document.body.removeChild(textarea);
   }
 };
 
