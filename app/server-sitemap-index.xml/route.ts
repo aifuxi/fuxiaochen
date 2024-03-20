ad2d5be6a29b1325dbe47a5f35d571ad1861feb6
fuxiaochen <aifuxi.js@gmail.com>
2024-03-20 15:58:00 +0800

    feat: 🎸 使用next-sitemap生成站点地图

---
+++ app/server-sitemap-index.xml/route.ts
@@ -0,0 +1,43 @@
+import { type ISitemapField, getServerSideSitemap } from 'next-sitemap';
+
+import { SITE_URL } from '@/config';
+
+import { PATHS } from '@/constants';
+import { prisma } from '@/lib/prisma';
+
+export async function GET() {
+  const blogs = await prisma.blog.findMany({
+    select: {
+      slug: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+    where: {
+      published: true,
+    },
+  });
+  const snippets = await prisma.snippet.findMany({
+    select: {
+      slug: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  const blogsSitemaps = blogs.map((item): ISitemapField => {
+    return {
+      loc: `${SITE_URL}${PATHS.SITE_BLOG}/${item.slug}`,
+      lastmod: new Date(item.updatedAt ?? item.createdAt).toISOString(),
+      changefreq: 'hourly',
+    };
+  });
+  const snippetsSitemaps = snippets.map((item): ISitemapField => {
+    return {
+      loc: `${SITE_URL}${PATHS.SITE_SNIPPET}/${item.slug}`,
+      lastmod: new Date(item.updatedAt ?? item.createdAt).toISOString(),
+      changefreq: 'hourly',
+    };
+  });
+
+  return getServerSideSitemap([...blogsSitemaps, ...snippetsSitemaps]);
+}
---
+++ next-sitemap.config.cjs
@@ -0,0 +1,21 @@
+const siteUrl = process.env.SITE_URL || 'https://fuxiaochen.com';
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl,
+  // 以/admin/*的路由不生成sitemap
+  exclude: ['/admin/*', '/auth/*', '/server-sitemap.xml'],
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        // 不允许爬虫爬取admin页面
+        disallow: ['/admin/', '/auth/'],
+      },
+    ],
+    additionalSitemaps: [`${siteUrl}/server-sitemap.xml`],
+  },
+};
