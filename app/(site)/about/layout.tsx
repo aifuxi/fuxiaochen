a19c7ad1954fa631b38387b9c30b0cf4af6bc9e7
fuxiaochen <aifuxi.js@gmail.com>
2024-03-21 00:20:15 +0800

    feat: 🎸 更新页面描述

--- app/(site)/about/layout.tsx
+++ app/(site)/about/layout.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 import { type Metadata } from 'next';
 
-import { PATHS, PATHS_MAP } from '@/constants';
+import { PATHS, PATHS_MAP, PATH_DESCRIPTION_MAP } from '@/constants';
 
 export const metadata: Metadata = {
   title: PATHS_MAP[PATHS.SITE_ABOUT],
+  description: PATH_DESCRIPTION_MAP[PATHS.SITE_ABOUT],
 };
 
 export default function Layout({ children }: React.PropsWithChildren) {
