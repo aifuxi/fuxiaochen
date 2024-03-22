baec088e13563b24778f873b4e74ba5bb6f5eefe
fuxiaochen <aifuxi.js@gmail.com>
2024-03-22 11:33:18 +0800

    fix: 🐛 表格cell设置最大宽度

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -132,7 +132,7 @@ export function DataTable<TData, TValue>({
     return table.getRowModel().rows.map((row) => (
       <TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
         {row.getVisibleCells().map((cell) => (
-          <TableCell key={cell.id} className="py-2">
+          <TableCell key={cell.id} className="py-2 max-w-[300px]">
             {flexRender(cell.column.columnDef.cell, cell.getContext())}
           </TableCell>
         ))}
