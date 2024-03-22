2d8b37d0ce060ab8ca9cdc7d03e01aa5079b2346
fuxiaochen <aifuxi.js@gmail.com>
2024-03-22 15:04:32 +0800

    feat: 🎸 支持Google登录

--- features/auth/actions/signin.ts
+++ features/auth/actions/signin.ts
@@ -1,12 +1,7 @@
 'use server';
 
-import { AuthError } from 'next-auth';
-
 import { PATHS } from '@/constants';
 import { signIn } from '@/lib/auth';
-import { prisma } from '@/lib/prisma';
-
-import { type SigninDTO, signinSchema } from '../types';
 
 export const signinWithGithub = async () => {
   await signIn('github', {
@@ -14,37 +9,8 @@ export const signinWithGithub = async () => {
   });
 };
 
-export const signinWithCredentials = async (params: SigninDTO) => {
-  const result = await signinSchema.safeParseAsync(params);
-
-  if (!result.success) {
-    const error = result.error.format()._errors?.join(';');
-    // TODO: 记录日志
-    throw new Error(error);
-  }
-
-  const isExist = await prisma.user.findUnique({
-    where: {
-      email: result.data.email,
-    },
+export const signinWithGoogle = async () => {
+  await signIn('google', {
+    redirectTo: PATHS.ADMIN_HOME,
   });
-
-  if (!isExist) {
-    // TODO: 记录日志
-    throw new Error('邮箱或密码错误');
-  }
-
-  try {
-    await signIn('credentials', {
-      ...result.data,
-      redirectTo: PATHS.ADMIN_HOME,
-    });
-  } catch (error) {
-    if (error instanceof AuthError) {
-      // TODO: 记录日志
-      throw new Error('邮箱或密码错误');
-    }
-
-    throw error;
-  }
 };
