6085ff6fddf0e8946b01254d7177fd6627eb33c1
fuxiaochen <aifuxi.js@gmail.com>
2024-03-28 23:27:55 +0800

    fix: 🐛 首页每60s重新验证数据，保证页面刷新

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,6 +2,8 @@ import { IntroScrollMouse } from '@/components/intro-scroll-mouse';
 
 import { HeroSection } from '@/features/home';
 
+export const revalidate = 60;
+
 export default function Page() {
   return (
     <div className="h-[calc(100vh-64px)] grid place-content-center relative">
