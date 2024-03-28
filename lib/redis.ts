aa4b19ed4fe3f2cc7f5408360ca10eb75e6e56e2
fuxiaochen <aifuxi.js@gmail.com>
2024-03-28 22:07:27 +0800

    feat: 🎸 自定义redis连接配置

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,15 +1,17 @@
 import Redis, { type Redis as RedisInstanceType } from 'ioredis';
 
-import { NODE_ENV } from '@/config';
+import { NODE_ENV, REDIS_HOST, REDIS_PORT } from '@/config';
+
+import { REDIS_KYE_PREFIX } from '@/constants';
 
 const globalForRedis = global as unknown as { redis: RedisInstanceType };
 
 export const redis =
   globalForRedis.redis ||
   new Redis({
-    host: '127.0.0.1',
-    port: 6379,
-    keyPrefix: 'fuxiaochen:',
+    host: REDIS_HOST || '127.0.0.1',
+    port: Number(REDIS_PORT) || 6379,
+    keyPrefix: REDIS_KYE_PREFIX,
   });
 
 if (NODE_ENV !== 'production') globalForRedis.redis = redis;
