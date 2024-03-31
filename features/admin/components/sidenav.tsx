b93a938db99a142b5f098c7a96f5c14ea266fa79
fuxiaochen <aifuxi.js@gmail.com>
2024-04-01 00:54:19 +0800

    feat: 🎸 侧边栏展示权限和回到前台按钮

--- features/admin/components/sidenav.tsx
+++ features/admin/components/sidenav.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { type Session } from 'next-auth';
+import Link from 'next/link';
 
-import { PLACEHODER_TEXT } from '@/constants';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+
+import { IconSolarArrowRightUpLinear } from '@/components/icons';
+
+import { PATHS, PLACEHODER_TEXT } from '@/constants';
 import { SignOutButton } from '@/features/auth';
 import { auth } from '@/lib/auth';
+import { isAdmin } from '@/lib/utils';
 
 import { Sidebar } from './sidebar';
+
+const Desc = ({ session }: { session: Session | null }) => {
+  return isAdmin(session?.user?.email) ? (
+    <div className="flex justify-center flex-col items-center space-y-1">
+      <Badge className="bg-background text-foreground">管理员</Badge>
+      <p className="text-xs text-muted-foreground font-medium">拥有所有权限</p>
+    </div>
+  ) : (
+    <div className="flex justify-center flex-col items-center space-y-1">
+      <Badge className="bg-background text-foreground">游客</Badge>
+      <p className="text-xs text-muted-foreground font-medium">
+        只能查看部分数据
+      </p>
+    </div>
+  );
+};
 
 export const Sidenav = async () => {
   const session = await auth();
@@ -23,11 +47,22 @@ export const Sidenav = async () => {
       <h4 className="hidden lg:block text-lg font-semibold tracking-tight mt-2 text-primary-foreground">
         {session?.user?.name ?? PLACEHODER_TEXT}
       </h4>
+      <Desc session={session} />
       <div className="w-full flex-col flex items-center mt-24 space-y-4">
         <Sidebar />
       </div>
-      <div className="mt-24 flex justify-center lg:grid w-full">
+      <div className="mt-24 flex justify-center lg:grid w-full space-y-1">
         <SignOutButton />
+
+        <Button
+          asChild
+          className="lg:!w-full text-primary-foreground bg-muted-foreground/10 hover:bg-muted-foreground/20"
+        >
+          <Link href={PATHS.SITE_HOME} target="_blank">
+            <span>去前台首页</span>
+            <IconSolarArrowRightUpLinear />
+          </Link>
+        </Button>
       </div>
     </aside>
   );
