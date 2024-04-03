7a2db336c55bc2ab521386d7d2bc2bd571d47402
fuxiaochen <aifuxi.js@gmail.com>
2024-04-03 15:13:27 +0800

    feat: 🎸 使用middleware控制后台页面访问权限

--- lib/auth.ts
+++ lib/auth.ts
@@ -12,6 +12,9 @@ import { prisma } from './prisma';
 
 export const { handlers, auth, signOut, signIn } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  // 解决这个错误：Error: PrismaClient is not configured to run in Vercel Edge Functions or Edge Middleware.
+  // 参考：https://github.com/prisma/prisma/issues/21310#issuecomment-1840428931
+  session: { strategy: 'jwt' },
   trustHost: true,
   providers: [
     // 允许多个account关联同一个user（email相同）
@@ -23,11 +26,20 @@ export const { handlers, auth, signOut, signIn } = NextAuth({
   },
   debug: NODE_ENV === 'development',
   callbacks: {
-    session: ({ session, token }) => {
+    session({ session, token }) {
       if (session.user && token?.sub) {
         session.user.id = token.sub;
       }
       return session;
+    },
+    authorized({ request, auth }) {
+      // 将来用作 Next.js middleware，如果是访问后台页面，校验是否登录
+      if (request.nextUrl.pathname.startsWith(PATHS.ADMIN_HOME)) {
+        return !!auth?.user;
+      }
+
+      // 其它路径直接放行
+      return true;
     },
   },
 });
---
+++ middleware.ts
@@ -0,0 +1,5 @@
+export { auth as middleware } from '@/lib/auth';
+
+export const config = {
+  matcher: `/admin/:path*`,
+};
