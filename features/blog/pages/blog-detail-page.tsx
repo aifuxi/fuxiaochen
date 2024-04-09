d539372353c02bbd0c64073d23ba367245fc0e24
fuxiaochen <aifuxi.js@gmail.com>
2024-04-10 00:20:13 +0800

    style: 💄 缩小博客和片段内容展示宽度，对齐微信公众号文章

--- features/blog/pages/blog-detail-page.tsx
+++ features/blog/pages/blog-detail-page.tsx
@@ -19,7 +19,7 @@ type BlogDetailProps = {
 export const BlogDetailPage = ({ blog, uv = 0 }: BlogDetailProps) => {
   return (
     <div className="md:max-w-screen-md 2xl:max-w-6xl md:px-0 md:mx-auto py-12 md:py-24 grid gap-9 px-6">
-      <article>
+      <article className="max-w-[678px] mx-auto">
         {blog.cover && (
           <img
             src={blog.cover}
