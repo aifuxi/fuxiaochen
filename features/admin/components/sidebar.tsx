6da2becdc80ab1682b8531d4351f6e247de1e500
fuxiaochen <aifuxi.js@gmail.com>
2024-05-11 09:11:57 +0800

    feat: clean code

--- features/admin/components/sidebar.tsx
+++ features/admin/components/sidebar.tsx
@@ -5,8 +5,6 @@ import React from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-import { Button } from '@/components/ui/button';
-
 import {
   IconSolarBook,
   IconSolarChartSquare,
