01d5b5aa22b003bf8bbe54e6dd46a6b9b51f11c0
fuxiaochen <aifuxi.js@gmail.com>
2024-05-18 15:40:13 +0800

    feat: 优化代码块展示

--- components/bytemd/plugins/shiki.ts
+++ components/bytemd/plugins/shiki.ts
@@ -1,9 +1,16 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
+import {
+  transformerMetaHighlight,
+  transformerMetaWordHighlight,
+  transformerNotationDiff,
+  transformerNotationHighlight,
+  transformerNotationWordHighlight,
+} from '@shikijs/transformers';
 import { type BytemdPlugin } from 'bytemd';
 import { load } from 'cheerio';
 import { isNil } from 'lodash-es';
 // 支持大部分语言高亮，体积更大
-import { codeToHtml } from 'shiki/bundle-full.mjs';
+import { bundledLanguages, codeToHtml } from 'shiki/bundle-full.mjs';
 
 // 仅支持部分语言高亮，体积更小
 // import { codeToHtml } from 'shiki/bundle-full.mjs';
@@ -21,7 +28,14 @@ export function shikiPlugin(): BytemdPlugin {
       const els = markdownBody.querySelectorAll<HTMLElement>('pre>code');
       if (els.length === 0) return;
       els.forEach(async (el) => {
-        const lang = el.className.replace('language-', '');
+        let lang = el.className.replace('language-', '');
+        // 当没有匹配到语言时，使用文本文件（txt也可以上plain或者text）兜底
+        // 参考：https://shiki.style/languages#special-languages
+        lang = Object.keys(bundledLanguages)
+          .map((el) => el.toLowerCase())
+          .includes(lang)
+          ? lang
+          : 'txt';
         el.className = `${el.className} shiki-code`;
         const codeGet = el.textContent || '';
 
@@ -34,6 +48,20 @@ export function shikiPlugin(): BytemdPlugin {
             light: 'vitesse-light',
             dark: 'vitesse-black',
           },
+          // transformer 有啥用看这个链接：https://shiki.style/packages/transformers#install
+          transformers: [
+            // diff 高亮
+            transformerNotationDiff(),
+            transformerNotationDiff(),
+
+            // 单词高亮
+            transformerNotationWordHighlight(),
+            transformerMetaWordHighlight(),
+
+            // 代码行高亮
+            transformerNotationHighlight(),
+            transformerMetaHighlight(),
+          ],
         });
 
         // 用cheerio解析codeToHtml生成的html字符串
