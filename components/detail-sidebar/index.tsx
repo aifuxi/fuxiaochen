a24a9a22111b1b2c1a096dfa2a1a83484f62851e
fuxiaochen <aifuxi.js@gmail.com>
2024-05-27 21:58:22 +0800

    feat: 博客和片段支持目录

---
+++ components/detail-sidebar/index.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React from 'react';
+
+import { cn } from '@/lib/utils';
+
+type DetailSidebarProps = React.PropsWithChildren;
+
+export const DetailSidebar = ({ children }: DetailSidebarProps) => {
+  return (
+    <div
+      className={cn(
+        'hidden h-fit pl-10 pr-4 py-10 sticky top-24',
+        'wrapper:block wrapper:w-[200px] ',
+        '2xl:w-[240px]',
+      )}
+    >
+      {children}
+    </div>
+  );
+};
