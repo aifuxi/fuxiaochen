89909348ac70046cf7a825c9e3a86392d6e4a32a
fuxiaochen <aifuxi.js@gmail.com>
2024-05-27 23:45:36 +0800

    feat: 笔记列表增加底部边距

--- features/admin/pages/note/admin-note-list-page.tsx
+++ features/admin/pages/note/admin-note-list-page.tsx
@@ -192,6 +192,9 @@ export const AdminNoteListPage = () => {
               ))}
         </Masonry>
       </ResponsiveMasonry>
+
+      {/* padding */}
+      <div className="h-32"></div>
     </AdminContentLayout>
   );
 
