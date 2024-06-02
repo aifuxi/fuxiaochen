fc108676dce915ec80c2e8b34c60c3a1e0306017
fuxiaochen <aifuxi.js@gmail.com>
2024-06-02 22:23:22 +0800

    refactor: 组件名称修改

--- components/icons/fa6-brands/icon-brand-github.tsx
+++ components/icons/fa6-brands/icon-brand-github.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { cn } from "@/lib/utils";
 
-export const IconBarandGithub = ({
+export const IconBrandGithub = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLSpanElement>) => {
