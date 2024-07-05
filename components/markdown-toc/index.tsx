0b61d8dc4651c975037faa13c7cb4ac1ea459856
fuxiaochen <aifuxi.js@gmail.com>
2024-07-05 22:56:44 +0800

    fix: 修复目录标题内容

--- components/markdown-toc/index.tsx
+++ components/markdown-toc/index.tsx
@@ -7,36 +7,43 @@ import Link from "next/link";
 import { useMount } from "ahooks";
 import { load } from "cheerio";
 
+import { type OptionItem } from "@/types";
+
 export const MarkdownTOC = () => {
-  const [headerIds, setHeaderIds] = React.useState<string[]>([]);
+  const [tocList, setTocList] = React.useState<OptionItem<string>[]>([]);
 
   useMount(() => {
     const markdownBodyElement = document.querySelector(".markdown-body")!;
     const $ = load(markdownBodyElement.innerHTML);
     const h2Elems = $("h2");
-    const ids = [];
     for (const h2 of h2Elems) {
-      const id = h2.attribs.id;
-      if (id) {
-        ids.push(id);
+      const h2Element = $(h2);
+      const text = h2Element.text();
+      const id = h2Element.attr("id");
+      if (text && id) {
+        setTocList((prev) => [
+          ...prev,
+          {
+            value: id,
+            label: text,
+          },
+        ]);
       }
     }
-
-    setHeaderIds(ids);
   });
 
   return (
     <div>
       <div>目录</div>
       <ul className="flex flex-col gap-2 pt-8 text-sm text-muted-foreground">
-        {headerIds.length > 0 ? (
-          headerIds.map((el) => (
-            <li key={el}>
+        {tocList.length > 0 ? (
+          tocList.map((el) => (
+            <li key={el.value}>
               <Link
-                href={`#${el}`}
+                href={`#${el.value}`}
                 className="line-clamp-1 text-ellipsis transition-colors hover:text-primary"
               >
-                {el}
+                {el.label}
               </Link>
             </li>
           ))
