2d458248b9f5b85f9ef580515ae0c5495326c659
fuxiaochen <aifuxi.js@gmail.com>
2024-07-05 22:43:12 +0800

    feat: /api路径不生成sitemap

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -3,8 +3,8 @@ const siteUrl = process.env.SITE_URL || "https://fuxiaochen.com";
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl,
-  // 以/admin/*的路由不生成sitemap
-  exclude: ["/admin/*", "/auth/*", "/server-sitemap.xml"],
+  // 以下路由不生成sitemap
+  exclude: ["/admin/*", "/auth/*", "/api/*", "/server-sitemap.xml"],
   generateRobotsTxt: true, // (optional)
   // ...other options
   robotsTxtOptions: {
