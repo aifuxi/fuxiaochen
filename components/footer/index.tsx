447460811cfe486e689a4874785152eaab44af5a
fuxiaochen <aifuxi.js@gmail.com>
2024-10-07 22:11:45 +0800

    feat: 移除底部导航列表

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -9,46 +9,14 @@ import {
   GONG_AN_NUMBER,
   ImageAssets,
   NICKNAME,
-  PATHS,
-  PATHS_MAP,
   SLOGAN,
-  navItems,
 } from "@/constants";
-import { cn } from "@/lib/utils";
 
 import { Wrapper } from "../wrapper";
 
 export const Footer = () => {
   return (
     <footer className="px-6 py-12">
-      <Wrapper
-        className={cn(
-          "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-x-24 gap-y-8 text-sm text-muted-foreground",
-        )}
-      >
-        <dl className="flex flex-col gap-3">
-          <dt className="text-lg font-semibold text-primary">导航</dt>
-          {navItems.map((el) => (
-            <dd key={el.link}>
-              <Link
-                href={el.link}
-                className="flex items-center transition-colors hover:font-semibold hover:text-primary"
-              >
-                {el.label}
-              </Link>
-            </dd>
-          ))}
-          <dd>
-            <Link
-              href={PATHS.SITEMAP}
-              className="transition-colors hover:font-semibold hover:text-primary"
-            >
-              {PATHS_MAP[PATHS.SITEMAP]}
-            </Link>
-          </dd>
-        </dl>
-      </Wrapper>
-
       <Wrapper className="flex flex-col items-center justify-center space-y-1 pt-24 text-sm text-muted-foreground md:flex-row md:space-x-4 md:space-y-0">
         <Link
           target="_blank"
