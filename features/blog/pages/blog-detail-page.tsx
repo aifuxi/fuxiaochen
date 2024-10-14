62e4fd958eab1dca8579fe8c4c472a14def832ea
fuxiaochen <aifuxi.js@gmail.com>
2024-10-15 00:33:11 +0800

    fix: 移动端预览博客和片段增加padding

--- features/blog/pages/blog-detail-page.tsx
+++ features/blog/pages/blog-detail-page.tsx
@@ -12,7 +12,7 @@ type BlogDetailProps = {
 
 export const BlogDetailPage = ({ blog }: BlogDetailProps) => {
   return (
-    <Wrapper className="flex !max-w-prose flex-col !px-0 pt-8">
+    <Wrapper className="flex !max-w-prose flex-col pt-8 md:!px-0">
       <h1 className="mb-6 break-all text-4xl font-semibold">{blog.title}</h1>
 
       <p className="mb-6 text-neutral-500">{blog.description}</p>
