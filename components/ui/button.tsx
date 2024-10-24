700d33e7177a703de8f450398c7bf2b1b4753ddf
fuxiaochen <aifuxi.js@gmail.com>
2024-10-24 22:08:56 +0800

    feat: icon button的圆角调整

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -24,7 +24,7 @@ const buttonVariants = cva(
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "size-10",
+        icon: "size-10 rounded-xl",
       },
     },
     defaultVariants: {
