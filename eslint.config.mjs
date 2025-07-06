c1dee4b62213d1a657385fe58a83d271d1aef51e
aifuxi <aifuxi.js@gmail.com>
2025-07-06 09:16:59 +0800

    feat: upgrade eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,62 +1,47 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-// @ts-check
-import eslint from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
+import { FlatCompat } from "@eslint/eslintrc";
+import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
 import eslintPluginCheckFile from "eslint-plugin-check-file";
-import eslintPluginImport from "eslint-plugin-import";
 import eslintPluginPrettier from "eslint-plugin-prettier";
-import eslintPluginReactHooks from "eslint-plugin-react-hooks";
-import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import { fileURLToPath } from "node:url";
+import { dirname } from "path";
 
-import eslintPluginTailwindcss from "eslint-plugin-tailwindcss";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const gitignorePath = fileURLToPath(new URL(".gitignore", import.meta.url));
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
-export default tseslint.config(
-  {
-    ignores: [
-      ".next/",
-      "**/node_modules",
-      "**/dist",
-      "**/build",
-      "**/public",
-      "**/.gitignore",
-      "**/pnpm-lock.yaml",
-      ".next/types/**/*.ts",
-      "next-env.d.ts",
-      "next.config.mjs",
-      ".prettierrc.cjs",
-      ".commitlintrc.cjs",
-      "postcss.config.cjs",
-      "next-sitemap.config.cjs",
-      "tailwind.config.ts",
-    ],
-  },
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-  tseslint.configs.stylisticTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
+export default defineConfig([
+  // 来自.gitignore文件的忽略规则，.gitignore中忽略的文件或文件夹，eslint也忽略
+  includeIgnoreFile(gitignorePath),
+
+  // next 官方推荐的规则
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+
   {
     plugins: {
-      eslintPluginReactHooks,
       eslintPluginCheckFile,
-      eslintPluginImport,
-      eslintPluginTailwindcss,
       eslintPluginPrettier,
     },
   },
+
   {
-    rules: {
-      "@typescript-eslint/restrict-template-expressions": [
-        "error",
-        {
-          allowNumber: true,
-        },
+    settings: {
+      "import-x/resolver-next": [
+        createTypeScriptImportResolver({
+          alwaysTryTypes: true, // Always try to resolve types under `<root>@types` directory even if it doesn't contain any source code, like `@types/unist`
+
+          bun: true, // Resolve Bun modules (https://github.com/import-js/eslint-import-resolver-typescript#bun)
+
+          // Choose from one of the "project" configs below or omit to use <root>/tsconfig.json or <root>/jsconfig.json by default
+
+          // Use <root>/path/to/folder/tsconfig.json or <root>/path/to/folder/jsconfig.json
+          project: "tsconfig.json",
+        }),
       ],
     },
   },
-);
+]);
