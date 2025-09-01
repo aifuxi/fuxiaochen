7178776d11fd2fae0ea44a4cef97929d13ea1bc3
aifuxi <aifuxi.js@gmail.com>
2025-09-02 00:29:34 +0800

    feat: 导航栏和博客列表和片段列表样式更新

--- features/blog/components/blog-list.tsx
+++ features/blog/components/blog-list.tsx
@@ -23,18 +23,12 @@ export const BlogList = ({ blogs }: BlogListProps) => {
   return (
     <ul
       className={`
-        grid grid-cols-1 gap-10
+        grid grid-cols-1 gap-4 gap-y-8
         md:grid-cols-2
       `}
     >
-      {blogs.map((el, idx) => (
-        <li
-          key={el.id}
-          className=""
-          style={{
-            animationDelay: `${(idx + 1) * 200}ms`,
-          }}
-        >
+      {blogs.map((el) => (
+        <li key={el.id} className="">
           <BlogListItem blog={el} />
         </li>
       ))}
