608f740e2dd4ba532734b0c80c19eb983bfa51c9
aifuxi <aifuxi.js@gmail.com>
2025-09-02 00:41:21 +0800

    feat: 关于页面边距

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -42,7 +42,7 @@ export default function Page() {
   return (
     <section
       className={`
-        mx-auto prose prose-neutral
+        mx-auto prose pt-8 prose-neutral
         dark:prose-invert
       `}
     >
