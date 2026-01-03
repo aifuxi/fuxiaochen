a6da8a6937a094eaa96fae6e64d9399bfe44aad5
aifuxi <aifuxi.js@gmail.com>
2026-01-03 16:06:38 +0800

    feat(admin): 支持单独修改 blog 的发布和精选状态

---
+++ web/apps/fxiaochen-admin/src/features/blog/components/blog-published-changer.tsx
@@ -0,0 +1,36 @@
+import { updateBlogPublished } from "@/api/blog";
+
+import { Switch } from "@douyinfe/semi-ui-19";
+import { useRequest } from "ahooks";
+
+interface Props {
+	currentPublished: boolean;
+	blogID: string;
+	onSuccess?: () => void;
+}
+
+export default function BlogPublishedChanger({
+	currentPublished,
+	blogID,
+	onSuccess,
+}: Props) {
+	const { loading, run } = useRequest(
+		(published: boolean) => updateBlogPublished(blogID, { published }),
+		{
+			manual: true,
+			onSuccess: () => {
+				onSuccess?.();
+			},
+		}
+	);
+
+	return (
+		<Switch
+			defaultChecked={currentPublished}
+			onChange={(v) => {
+				run(v);
+			}}
+			loading={loading}
+		/>
+	);
+}
