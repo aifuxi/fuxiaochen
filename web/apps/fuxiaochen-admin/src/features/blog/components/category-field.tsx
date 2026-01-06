70a2b7193faafd6dedbb0ef123a1c08f055f8db6
aifuxi <aifuxi.js@gmail.com>
2026-01-06 22:19:58 +0800

    feat(admin): 支持在创建博客的时候新建标签和分类

--- web/apps/fuxiaochen-admin/src/features/blog/components/category-field.tsx
+++ web/apps/fuxiaochen-admin/src/features/blog/components/category-field.tsx
@@ -1,92 +1,124 @@
-import { Form, useFieldApi } from "@douyinfe/semi-ui-19";
+import { Button, Form, useFieldApi } from "@douyinfe/semi-ui-19";
 import { useMount, useRequest } from "ahooks";
 import { getCategoryDetail, getCategoryList } from "@/api/category";
 import { useState } from "react";
+import CategoryCreateModal from "@/features/category/components/category-create-modal";
+import { IconRefresh } from "@douyinfe/semi-icons";
+import NiceModal from "@ebay/nice-modal-react";
 
 interface OptionItemType {
-  value: string;
-  label: string;
+	value: string;
+	label: string;
 }
 
 type CategoryFieldProps = Omit<
-  React.ComponentProps<typeof Form.Select>,
-  "optionList" | "loading"
+	React.ComponentProps<typeof Form.Select>,
+	"optionList" | "loading"
 >;
 
 export function CategoryField(props: CategoryFieldProps) {
-  const fieldApi = useFieldApi(props.field);
-  const [options, setOptions] = useState<OptionItemType[]>([]);
-  const [categoryIDs, setCategoryIDs] = useState<string[]>([]);
+	const fieldApi = useFieldApi(props.field);
+	const [options, setOptions] = useState<OptionItemType[]>([]);
+	const [categoryIDs, setCategoryIDs] = useState<string[]>([]);
 
-  const { loading: detailLoading } = useRequest(
-    () => Promise.all(categoryIDs.map((id) => getCategoryDetail(id))),
-    {
-      ready: Boolean(categoryIDs?.length),
-      debounceWait: 500,
-      onSuccess(resp) {
-        setOptions(
-          resp.map(
-            (el): OptionItemType => ({
-              label: el?.data?.name,
-              value: el?.data?.id,
-            })
-          )
-        );
-      },
-    }
-  );
+	const { loading: detailLoading } = useRequest(
+		() => Promise.all(categoryIDs.map((id) => getCategoryDetail(id))),
+		{
+			ready: Boolean(categoryIDs?.length),
+			debounceWait: 500,
+			onSuccess(resp) {
+				setOptions(
+					resp.map(
+						(el): OptionItemType => ({
+							label: el?.data?.name,
+							value: el?.data?.id,
+						})
+					)
+				);
+			},
+		}
+	);
 
-  const { loading, run } = useRequest(
-    (v: string) =>
-      getCategoryList({
-        page: 1,
-        pageSize: 100,
-        name: v,
-      }),
-    {
-      manual: true,
-      onSuccess(resp) {
-        if (resp?.data?.lists) {
-          setOptions(
-            resp.data.lists.map(
-              (el): OptionItemType => ({ label: el.name, value: el.id })
-            )
-          );
-        } else {
-          setOptions([]);
-        }
-      },
-    }
-  );
+	const { loading, run, refresh } = useRequest(
+		(v: string) =>
+			getCategoryList({
+				page: 1,
+				pageSize: 100,
+				name: v,
+			}),
+		{
+			manual: true,
+			onSuccess(resp) {
+				if (resp?.data?.lists) {
+					setOptions(
+						resp.data.lists.map(
+							(el): OptionItemType => ({
+								label: el.name,
+								value: el.id,
+							})
+						)
+					);
+				} else {
+					setOptions([]);
+				}
+			},
+		}
+	);
 
-  useMount(() => {
-    run("");
-    setTimeout(() => {
-      const categoryIDs = props.multiple
-        ? (fieldApi.getValue() as string[])
-        : [fieldApi.getValue() as string].filter(Boolean);
-      if (categoryIDs?.length) {
-        setCategoryIDs(categoryIDs);
-      }
-    }, 1000);
-  });
+	useMount(() => {
+		run("");
+		setTimeout(() => {
+			const categoryIDs = props.multiple
+				? (fieldApi.getValue() as string[])
+				: [fieldApi.getValue() as string].filter(Boolean);
+			if (categoryIDs?.length) {
+				setCategoryIDs(categoryIDs);
+			}
+		}, 1000);
+	});
 
-  const handleSearch = (inputValue: string) => {
-    run(inputValue);
-  };
+	const handleSearch = (inputValue: string) => {
+		run(inputValue);
+	};
 
-  return (
-    <Form.Select
-      size="large"
-      placeholder="请选择博客分类"
-      className="w-full"
-      filter
-      remote
-      onSearch={handleSearch}
-      optionList={options}
-      loading={loading || detailLoading}
-      emptyContent={null}
-      {...props}
-    ></Form.Select>
-  );
+	return (
+		<Form.Select
+			size="large"
+			placeholder="请选择博客分类"
+			className="w-full"
+			filter
+			remote
+			onSearch={handleSearch}
+			optionList={options}
+			loading={loading || detailLoading}
+			emptyContent={null}
+			suffix={
+				<Button
+					onClick={(e) => {
+						e.stopPropagation();
+						NiceModal.show(CategoryCreateModal, {
+							onSuccess: refresh,
+						});
+					}}
+				>
+					新建分类
+				</Button>
+			}
+			extraText={
+				<div className="inline-flex items-center">
+					<Button
+						link
+						theme="borderless"
+						icon={<IconRefresh />}
+						onClick={() => {
+							refresh();
+						}}
+					>
+						刷新数据
+					</Button>
+				</div>
+			}
+			{...props}
+		></Form.Select>
+	);
 }
