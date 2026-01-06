ac51335e9b0f6b87a625d544c0931266817daae4
aifuxi <aifuxi.js@gmail.com>
2026-01-06 21:57:38 +0800

    fix(portal): 删除多余接口

--- web/apps/fuxiaochen-portal/api/tag.ts
+++ web/apps/fuxiaochen-portal/api/tag.ts
@@ -1,7 +1,6 @@
 import type {
   CommonResponse,
   Tag,
-  TagCreateReq,
   TagListReq,
   TagListResp,
 } from "fuxiaochen-types";
@@ -21,29 +20,6 @@ export async function getTagList(req: TagListReq) {
 /** 获取标签详情 */
 export async function getTagDetail(tagID: string) {
   const res = await request.get<CommonResponse<Tag>>(
-    `${API_BASE_PATH}/${tagID}`,
-  );
-  return res.data;
-}
-
-/** 创建标签 */
-export async function createTag(req: TagCreateReq) {
-  const res = await request.post<CommonResponse<Tag>>(API_BASE_PATH, req);
-  return res.data;
-}
-
-/** 更新标签 */
-export async function updateTag(tagID: string, req: TagCreateReq) {
-  const res = await request.put<CommonResponse<Tag>>(
-    `${API_BASE_PATH}/${tagID}`,
-    req,
-  );
-  return res.data;
-}
-
-/** 删除标签 */
-export async function deleteTag(tagID: string) {
-  const res = await request.delete<CommonResponse<void>>(
     `${API_BASE_PATH}/${tagID}`,
   );
   return res.data;
