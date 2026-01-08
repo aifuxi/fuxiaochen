b737075fb1d24553b18bbc714cd870d6054a7cbb
aifuxi <aifuxi.js@gmail.com>
2026-01-08 21:11:52 +0800

    feat(portal): 更新logo

--- web/apps/fuxiaochen-portal/app/layout.tsx
+++ web/apps/fuxiaochen-portal/app/layout.tsx
@@ -26,24 +26,7 @@ export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <html suppressHydrationWarning lang="zh-CN">
       <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" type="image/svg+xml" href="/images/fuxiaochen-logo.svg" />
         {/* Google Search Console 验证 */}
         <meta
           name="google-site-verification"
