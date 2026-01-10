ca6eea0c142c254d37cd3bbee87ef810f4710a28
aifuxi <aifuxi.js@gmail.com>
2026-01-10 12:02:10 +0800

    chore: 更新表字段 comment

--- internal/model/changelog.go
+++ internal/model/changelog.go
@@ -5,5 +5,5 @@ type Changelog struct {
 
 	Version string `gorm:"size:50;not null;comment:版本号" json:"version"`
 	Content string `gorm:"type:text;not null;comment:更新内容" json:"content"`
-	Date    int    `gorm:"comment:发布日期" json:"date"`
+	Date    int    `gorm:"comment:手动指定的发布日期" json:"date"`
 }
