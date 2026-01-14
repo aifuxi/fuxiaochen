75947a3aa2014c3a4098832126cc922c9e98c505
aifuxi <aifuxi.js@gmail.com>
2026-01-14 23:53:45 +0800

    feat: 使用 env 作为配置文件

--- config/config.go
+++ config/config.go
@@ -7,60 +7,64 @@ import (
 )
 
 type Config struct {
-	Server   ServerConfig   `mapstructure:"server"`
-	Database DatabaseConfig `mapstructure:"database"`
-	JWT      JWTConfig      `mapstructure:"jwt"`
-	Log      LogConfig      `mapstructure:"log"`
-	OSS      OSSConfig      `mapstructure:"oss"`
+	Server   ServerConfig   `mapstructure:",squash"`
+	Database DatabaseConfig `mapstructure:",squash"`
+	JWT      JWTConfig      `mapstructure:",squash"`
+	Log      LogConfig      `mapstructure:",squash"`
+	OSS      OSSConfig      `mapstructure:",squash"`
 }
 
 type ServerConfig struct {
-	Host    string `mapstructure:"host"`
-	Port    int    `mapstructure:"port"`
-	Mode    string `mapstructure:"mode"`
-	Version string `mapstructure:"version"`
+	Port    int    `mapstructure:"server_port"`
+	Mode    string `mapstructure:"server_mode"`
+	Version string `mapstructure:"server_version"`
 }
 
 type DatabaseConfig struct {
-	Host     string `mapstructure:"host"`
-	Port     int    `mapstructure:"port"`
-	User     string `mapstructure:"user"`
-	Password string `mapstructure:"password"`
-	DBName   string `mapstructure:"dbname"`
+	Host     string `mapstructure:"database_host"`
+	Port     int    `mapstructure:"database_port"`
+	User     string `mapstructure:"database_user"`
+	Password string `mapstructure:"database_password"`
+	DBName   string `mapstructure:"database_dbname"`
 }
 
 type JWTConfig struct {
-	Secret string `mapstructure:"secret"`
-	Expire int    `mapstructure:"expire"`
+	Secret string `mapstructure:"jwt_secret"`
+	Expire int    `mapstructure:"jwt_expire"`
 }
 
 type LogConfig struct {
-	Level      string `mapstructure:"level"`
-	Filename   string `mapstructure:"filename"`
-	MaxSize    int    `mapstructure:"max_size"`
-	MaxAge     int    `mapstructure:"max_age"`
-	MaxBackups int    `mapstructure:"max_backups"`
-	Compress   bool   `mapstructure:"compress"`
+	Level      string `mapstructure:"log_level"`
+	Filename   string `mapstructure:"log_filename"`
+	MaxSize    int    `mapstructure:"log_max_size"`
+	MaxAge     int    `mapstructure:"log_max_age"`
+	MaxBackups int    `mapstructure:"log_max_backups"`
+	Compress   bool   `mapstructure:"log_compress"`
 }
 
 type OSSConfig struct {
-	AccessKeyID     string `mapstructure:"access_key_id"`
-	AccessKeySecret string `mapstructure:"access_key_secret"`
-	Region          string `mapstructure:"region"`
-	Bucket          string `mapstructure:"bucket"`
-	UploadDir       string `mapstructure:"upload_dir"`
+	AccessKeyID     string `mapstructure:"oss_access_key_id"`
+	AccessKeySecret string `mapstructure:"oss_access_key_secret"`
+	Region          string `mapstructure:"oss_region"`
+	Bucket          string `mapstructure:"oss_bucket"`
+	UploadDir       string `mapstructure:"oss_upload_dir"`
 }
 
 var AppConfig *Config
 
 func Init() error {
-	viper.SetConfigName("app")
-	viper.SetConfigType("yml")
-	viper.AddConfigPath("config")
+	viper.SetConfigName(".env")
+	viper.SetConfigType("env")
 	viper.AddConfigPath(".")
 
+	// Allow reading from environment variables (important if .env is missing or overridden)
+	viper.AutomaticEnv()
+
 	if err := viper.ReadInConfig(); err != nil {
-		return fmt.Errorf("error reading config file: %w", err)
+		// It's okay if config file doesn't exist, we might be relying on env vars
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return fmt.Errorf("error reading config file: %w", err)
+		}
 	}
 
 	if err := viper.Unmarshal(&AppConfig); err != nil {
