7aebe83a8845c951aa0306eae3f1de3ef7ce0094
aifuxi <aifuxi.js@gmail.com>
2026-01-29 22:17:19 +0800

    feat: clean workspace

--- internal/model/changelog.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type Changelog struct {
-	CommonModel
-
-	Version string `gorm:"size:50;not null;comment:版本号" json:"version"`
-	Content string `gorm:"type:text;not null;comment:更新内容" json:"content"`
-	Date    int    `gorm:"comment:手动指定的发布日期" json:"date"`
-}
--- internal/model/token.go
+++
@@ -1,7 +0,0 @@
-package model
-
-type Token struct {
-	CommonModel
-	Token  string `gorm:"column:token"`
-	UserID int64  `gorm:"column:user_id"`
-}
