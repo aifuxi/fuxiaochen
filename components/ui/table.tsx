e3f330f1f5a94554824037638166c31a5dc2e7ba
aifuxi <aifuxi.js@gmail.com>
2026-02-18 00:32:49 +0800

    feat(table): 添加固定列样式支持

--- components/ui/table.tsx
+++ components/ui/table.tsx
@@ -77,9 +77,11 @@ function TableHead({ className, ...props }: React.ComponentProps<"th">) {
       data-slot="table-head"
       className={cn(
         `
-          h-10 px-2 text-left align-middle font-medium whitespace-nowrap text-text-secondary
+          sticky h-10 bg-surface px-2 text-left align-middle font-medium whitespace-nowrap text-text-secondary
           [&:has([role=checkbox])]:pr-0
           [&>[role=checkbox]]:translate-y-[2px]
+          [&[data-pinned=left]]:left-0
+          [&[data-pinned=right]]:right-0 [&[data-pinned=right]]:shadow-[-4px_0_8px_-4px_rgba(0,0,0,0.1)]
         `,
         className,
       )}
@@ -94,9 +96,11 @@ function TableCell({ className, ...props }: React.ComponentProps<"td">) {
       data-slot="table-cell"
       className={cn(
         `
-          p-2 align-middle whitespace-nowrap
+          sticky bg-surface p-2 align-middle whitespace-nowrap
           [&:has([role=checkbox])]:pr-0
           [&>[role=checkbox]]:translate-y-[2px]
+          [&[data-pinned=left]]:left-0
+          [&[data-pinned=right]]:right-0 [&[data-pinned=right]]:shadow-[-4px_0_8px_-4px_rgba(0,0,0,0.1)]
         `,
         className,
       )}
