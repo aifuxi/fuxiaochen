835613354a0d370b500e651baeb32babb0c70c97
aifuxi <aifuxi.js@gmail.com>
2026-03-28 10:40:57 +0800

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
 import "@/styles/global.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -6,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning>
+    <html suppressHydrationWarning className={cn("font-sans", geist.variable)}>
       <head>
         <link
           rel="apple-touch-icon"
