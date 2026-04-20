fe7dc990a2a9383a86caf80f4aa2f0a5bdb9f8ef
aifuxi <aifuxi.js@gmail.com>
2026-04-20 23:13:36 +0800

    chore: clean workspace

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,3 @@
-import type { Metadata } from "next";
-import { DM_Mono, Inter } from "next/font/google";
 import Script from "next/script";
 
 import { GoogleAnalytics } from "@next/third-parties/google";
@@ -9,38 +7,7 @@ import { ModalProvider } from "@/components/modal-provider";
 
 import { isProduction } from "@/lib/env";
 
-import { NICKNAME, SLOGAN, WEBSITE } from "@/constants/info";
-
 import "@/styles/global.css";
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
-
-const dmMono = DM_Mono({
-  subsets: ["latin"],
-  variable: "--font-dm-mono",
-  weight: ["400", "500"],
-});
-
-export const metadata: Metadata = {
-  title: {
-    template: `%s | ${WEBSITE}`,
-    default: WEBSITE,
-  },
-  description: SLOGAN,
-  keywords: NICKNAME,
-};
-
-export const viewport = {
-  width: "device-width",
-  initialScale: 1.0,
-  maximumScale: 1.0,
-  minimumScale: 1.0,
-  userScalable: false,
-  viewportFit: "cover",
-};
 
 export default function RootLayout({
   children,
@@ -48,26 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="zh-CN" className="scroll-smooth" suppressHydrationWarning>
+    <html lang="zh-CN">
       <head>
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-icon.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="icon" type="image/svg+xml" href="/logo.svg" />
         {/* Google Search Console 验证 */}
         {isProduction() &&
           process.env.NEXT_PUBLIC_GOOGLE_SEARCH_CONSOLE_CONTENT && (
@@ -77,9 +27,7 @@ export default function RootLayout({
             />
           )}
       </head>
-      <body
-        className={`${inter.variable} ${dmMono.variable} bg-canvas font-sans text-text-strong antialiased`}
-      >
+      <body className="antialiased">
         <ModalProvider>{children}</ModalProvider>
         <Toaster richColors position="top-center" />
       </body>
