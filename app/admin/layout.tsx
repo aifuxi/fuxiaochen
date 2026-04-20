af69d117b41dfbb15f889972f6188c9d88d53a89
aifuxi <aifuxi.js@gmail.com>
2026-04-20 23:13:36 +0800

    chore: clean workspace

--- app/admin/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AdminShell } from "@/components/admin/admin-shell";
-
-export default function AdminLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <AdminShell>{children}</AdminShell>;
-}
